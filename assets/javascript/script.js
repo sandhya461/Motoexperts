f107cd908bbda3029df4251553f4100b483607de
sandhya <sandhya.rampurek@gmail.com>
2021-04-02 23:43:56 -0700

    motoexperts project commit

---
+++ assets/javascript/script.js
@@ -0,0 +1,19 @@
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+// counter section
+$(document).ready(function() {
+    $('.counter').each(function() {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function(now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+});
